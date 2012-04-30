93ec76a0f9a080331db98dceacc0d773e00aebbe
Brent Vatne <brentvatne@gmail.com>
2012-04-30 13:17:59 -0700

    restyle a bit and add more actions

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -8,17 +8,27 @@ module StarterApp
       erb 'decks/index', :locals => {:decks => decks}
     end
 
-    get '/decks/new', :authenticates => true do
-      erb 'decks/new'
-    end
-
     post '/decks', :authenticates => true do
       current_user.new_deck(:name => params[:name])
       redirect to('decks')
     end
 
+    get '/decks/new', :authenticates => true do
+      erb 'decks/new'
+    end
+
+    get '/decks/:id/edit', :authenticates => true do
+      # check if it really belongs to this user
+      erb 'decks/edit'
+    end
+
+    get '/decks/:id' do
+
+    end
+
     post '/decks/:id/slides', :authenticates => true do
 
     end
+
   end
 end
