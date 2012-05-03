5a199f68a3ab176310b9e6b6c82c26b17a52488b
Brent Vatne <brentvatne@gmail.com>
2012-05-03 13:13:15 -0700

    able to re-arrange slides

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -5,11 +5,13 @@ module DeckApp
 
     get '/decks', :authenticates => true do
       decks = current_user.decks
+
       erb 'decks/index', :locals => {:decks => decks}
     end
 
     post '/decks', :authenticates => true do
       current_user.create_deck(:name => params[:name])
+
       redirect to('decks')
     end
 
@@ -18,34 +20,72 @@ module DeckApp
     end
 
     get '/decks/:id/edit', :authenticates => true do
-      # check if it really belongs to this user
-      deck = Deck.first(:id => params[:id])
+      deck = current_user.decks.first(:id => params[:id])
+
       erb 'decks/edit', :locals => {:deck => deck}
     end
 
     get '/decks/:id/slides/new', :authenticates => true do
-      deck = Deck.first(:id => params[:id])
+      deck = current_user.decks.first(:id => params[:id])
+
       erb 'slides/new', :locals => {:deck => deck}
     end
 
     post '/decks/:id/slides', :authenticates => true do
-      deck = Deck.first(:id => params[:id])
+      deck = current_user.decks.first(:id => params[:id])
       deck.create_slide(params[:content])
+
       redirect to("/decks/#{params[:id]}/edit")
     end
 
-    get '/slides/:id/edit' do
+    get '/slides/:id/edit', :authenticates => true do
       slide = Slide.first(:id => params[:id])
       deck  = slide.deck
+
       erb 'slides/edit', :locals => {:deck => deck, :slide => slide}
     end
 
-    post '/slides/:id' do
+    post '/slides/:id', :authenticates => true  do
       slide = Slide.first(:id => params[:id])
       slide.content = params[:content]
       slide.save
+
       redirect to("/slides/#{slide.id}/edit")
     end
+
+    # ***************************
+    #  Move, edit, delete slides
+    # ***************************
+
+    get '/decks/:deck_id/slides/:slide_id/move-left', :authenticates => true do
+      deck  = current_user.decks.first(:id => params[:deck_id])
+      slide = deck.slides.first(:id => params[:slide_id])
+
+      deck.move_slide_left(slide)
+
+      redirect to("/decks/#{deck.id}/edit")
+    end
+
+    get '/decks/:deck_id/slides/:slide_id/move-right', :authenticates => true do
+      deck  = current_user.decks.first(:id => params[:deck_id])
+      slide = deck.slides.first(:id => params[:slide_id])
+
+      deck.move_slide_right(slide)
+
+      redirect to("/decks/#{deck.id}/edit")
+    end
+
+    get '/decks/:deck_id/slides/:slide_id/delete', :authenticates => true do
+      deck  = current_user.decks.first(:id => params[:deck_id])
+      slide = deck.slides.first(:id => params[:slide_id])
+
+      deck.delete_slide(slide)
+    end
+
+
+    # ****************************
+    #       Unimplemented
+    # ****************************
 
     post '/preview' do
       params[:content] # convert markdown to html
@@ -54,5 +94,6 @@ module DeckApp
     get '/decks/:id' do
       # actual slideshow here
     end
+
   end
 end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -18,6 +18,34 @@ module DeckApp
       (slides.max(:number) || 0) + 1
     end
 
+    def slides
+      super.all(:order => [ :number.asc ])
+    end
+
+    def move_slide_left(slide)
+      unless slide == slides.first
+        swap_slides(slide, slides.first(:number => slide.number - 1))
+      end
+    end
+
+    def move_slide_right(slide)
+      unless slide == slides.last
+        swap_slides(slide, slides.first(:number => slide.number + 1))
+      end
+    end
+
+    def swap_slides(first_slide, second_slide)
+      first_number  = first_slide.number
+      second_number = second_slide.number
+
+      first_slide.number  = second_number
+      second_slide.number = first_number
+
+      # should be a transaction
+      first_slide.save
+      second_slide.save
+    end
+
     def delete_slide
       # move the numbers accordingly
     end
