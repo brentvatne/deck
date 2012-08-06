8c600f3a0395ce155fafc8ca4be2a931c407a35d
Brent Vatne <brentvatne@gmail.com>
2012-08-06 09:19:25 -0700

    remove useless code

--- app/app.rb
+++ app/app.rb
@@ -2,7 +2,6 @@ require 'sinatra/base'
 require 'padrino-helpers'
 require 'sinatra/flash'
 require 'sass'
-require 'coffee-script'
 require_relative '../config/api_credentials'
 
 module DeckApp
@@ -17,9 +16,9 @@ module DeckApp
     set :client_secret, DeckApp::GAPI_CLIENT_SECRET
 
     enable :static
-    set :root,           File.dirname(__FILE__)
-    set :assets,         File.dirname(__FILE__) + '/assets/'
-    set :public_folder,  settings.assets + '../public'
+    set    :root,           File.dirname(__FILE__)
+    set    :assets,         File.dirname(__FILE__) + '/assets/'
+    set    :public_folder,  settings.assets + '../public'
 
     helpers do
       def json_params
@@ -38,20 +37,6 @@ module DeckApp
     end
 
     private
-
-    # Checks the disk to see if the given filename exists as a scss spreadsheet
-    #
-    # Returns true if it does exist, false if not
-    def stylesheet_exists?(asset)
-      File.exists?(File.join(settings.root, settings.scss_dir, asset + ".scss"))
-    end
-
-    # Checks the disk to see if the given filename exists as coffeescript
-    #
-    # Returns true if it does exist, false if not
-    def coffeescript_exists?(asset)
-      File.exists?(File.join(settings.root, settings.coffee_dir, asset + ".coffee"))
-    end
 
     def erb(file, *args)
       super(file.to_sym, *args)
